eee00d08e48f91bed2f87f9ca47c9ccf9e705b21
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2023-07-28 16:10:39 -0300

    Initial commit

---
+++ tecnt/tecnt/src/main/kotlin/com/tecnt/tecnt/TecntApplication.kt
@@ -0,0 +1,11 @@
+package com.tecnt.tecnt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TecntApplication
+
+fun main(args: Array<String>) {
+	runApplication<TecntApplication>(*args)
+}
---
+++ tecnt/tecnt/src/test/kotlin/com/tecnt/tecnt/TecntApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tecnt.tecnt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TecntApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
